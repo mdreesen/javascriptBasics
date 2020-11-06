88b594492d0f7b1c66ed834cb6655b7d840923f0
Michael Dreesen <michaeldreesen90@gmail.com>
2020-11-06 15:32:24 -0700

    added variables.js

---
+++ variables.js
@@ -0,0 +1,13 @@
+// Rule 1: cannot define a variable more than 1x
+let petName = 'Arrow';
+// changes value of petName to another value
+petName = 'Spot'
+console.log(petName);
+
+// Rule 2: There are rules related to variables names
+let a1$ = 2;
+let result = a1$ + 4;
+
+// 3: variables names cannot be reserved keywords.
+// let let = 12
+// The above is not valid
