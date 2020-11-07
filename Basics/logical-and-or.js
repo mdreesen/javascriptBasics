608474fcaad0290b6b55d00da813cf15b8aefa54
Michael Dreesen <michaeldreesen90@gmail.com>
2020-11-06 17:04:55 -0700

    added logical-and-or.js file to practice

---
+++ Basics/logical-and-or.js
@@ -0,0 +1,12 @@
+let temp = 150;
+
+// logical and operator - True if both sides are true. False otherwise
+if (temp >= 60 && temp <= 90) {
+    console.log('It is pretty nice out!')
+};
+
+// Logical or operator - true if at least on side is true. False otherwise
+if (temp <= 0 || temp >= 110) {
+    console.log('do not go outside')
+};
+//////////////////////////////////////////////////////////////////////////////////////////////////
