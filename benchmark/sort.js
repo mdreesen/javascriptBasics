871f18bde1911dfbf35e9ca3ebba90be98f5b380
Michael Dreesen <michaeldreesen90@gmail.com>
2020-11-11 20:55:46 -0700

    added factory functions to basics directory

--- benchmark/sort.js
+++ benchmark/sort.js
@@ -1,6 +1,5 @@
 const numbers = [54, 8, 87, 88, 100, 98, 17, 72, 67, 42, 40, 34, 55, 18, 33, 94, 59, 68, 92, 2];
 
-
 const bubbleSort = (arr) => {
     let sorted = false;
 
@@ -26,7 +25,30 @@ const bubbleSort = (arr) => {
     return arr;
 };
 
-module.exports = { bubbleSort };
+
+const quickSort = (arr) => {
+    // use first index as the pivot point
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    // start at index 1 to ignore pivot
+    for (let i = 1; i < arr.length; i++) {
+        // push into different arrays based on value compared to pivot
+        if (arr[i] <= pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    // merge arrays and pivot together
+    return left.concat(pivot, right);
+};
+
+// export along with bubble sort
+module.exports = { bubbleSort, quickSort };
+
 
 /*
 const benchmark = require('benchmark');
